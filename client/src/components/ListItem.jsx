36ddc76f405fb11df20bbb26e93d29c08f6a0104
James Byrd <jd3yrd@gmail.com>
2018-08-07 05:30:18 -0400

    add loading message

--- client/src/components/ListItem.jsx
+++ client/src/components/ListItem.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 
-const ListItem = (props) => (
+const ListItem = (props) => {
+  if(props.book.title === 'Loading'){
+    return (
+      <div className = "book">
+        Loading Books Please Wait
+      </div>
+    )
+  }
+  return (
   <div className = "book">
     <a href={props.book.url}><img src={props.book.img}/></a>
     <div className = "text">
@@ -8,6 +16,8 @@ const ListItem = (props) => (
       <span> by {props.book.author}</span>
     </div>
   </div>
-)
+  )
+}
+
 
 export default ListItem;
